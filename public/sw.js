9303cd059571fc53ec2ec24f21c33971d27da0ae
Diogo <dabranches@gmail.com>
2026-08-07 09:49:36 +0100

    feat: load all filtered movements

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='reconciliacao-shell-v2.1.6';
+const CACHE='reconciliacao-shell-v2.1.7';
 const SHELL=['/','/manifest.webmanifest','/favicon.svg','/keve-logo-green.png','/keve-logo-purple.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));self.clients.claim();});
